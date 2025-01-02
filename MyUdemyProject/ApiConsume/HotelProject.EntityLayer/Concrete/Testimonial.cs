cf60a0af6846fa962fd5fc3e04988e4b62bf25cb
gaye armut <gayearmut@gmail.com>
2025-01-02 17:23:09 +0300

    creating entity

---
+++ MyUdemyProject/ApiConsume/HotelProject.EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.EntityLayer.Concrete
+{
+	public class Testimonial
+	{
+        public int TestimonialID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Descripton { get; set; }
+        public string Image { get; set; }
+    }
+}
