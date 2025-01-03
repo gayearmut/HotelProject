1dbb5307b8ef771da217a3da644621e40ab41ee9
gaye armut <gayearmut@gmail.com>
2025-01-03 14:29:03 +0300

    references and migration

---
+++ MyUdemyProject/ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using HotelProject.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Concrete
+{
+	public class Context:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=DESKTOP-OHLH4MB; initial catalog=ApiDb;integrated security=true");//bağlantı adresini yazmak için kullandığımız metod
+		}
+
+        public DbSet<Room> Rooms { get; set; }//sol taraf sınıf ismi sağ taraf sql'e yansıyacak tablo ismi
+
+		public DbSet<Service> Services { get; set; }
+		public DbSet<Staff> Staffs { get; set; }
+		public DbSet<Subscribe> Subscribes { get; set; }
+		public DbSet<Testimonial> Testimonials { get; set; }
+	}
+}
