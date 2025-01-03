8403ab3b88794397addc946cc7334432fd8342ed
gaye armut <gayearmut@gmail.com>
2025-01-03 15:51:14 +0300

    add generic repository and entityframework classes

---
+++ MyUdemyProject/ApiConsume/HotelProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,48 @@
+﻿using HotelProject.DataAccessLayer.Abstract;
+using HotelProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Repositories
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class
+	{
+		private readonly Context context;
+
+		public GenericRepository(Context context)
+		{
+			this.context = context;
+		}
+
+		public void Delete(T t)
+		{
+			context.Remove(t);
+			context.SaveChanges();
+		}
+
+		public T GetByID(int id)
+		{
+			return context.Set<T>().Find(id);//id'ye göre bulma
+		}
+
+		public List<T> GetList()
+		{
+			return context.Set<T>().ToList();//listeleme
+		}
+
+		public void Insert(T t)
+		{
+			context.Add(t);
+			context.SaveChanges();
+		}
+
+		public void Update(T t)
+		{
+			context.Update(t);
+			context.SaveChanges();
+		}
+	}
+}
