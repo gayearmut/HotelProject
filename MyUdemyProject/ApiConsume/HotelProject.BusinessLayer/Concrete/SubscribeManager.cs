890d8330c99298bacf7fc89a35a3e61e4719ce60
gaye armut <gayearmut@gmail.com>
2025-05-01 11:14:14 +0300

    layout preparaion completed

--- MyUdemyProject/ApiConsume/HotelProject.BusinessLayer/Concrete/SubscribeManager.cs
+++ MyUdemyProject/ApiConsume/HotelProject.BusinessLayer/Concrete/SubscribeManager.cs
@@ -1,39 +1,42 @@
 ﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
 using HotelProject.EntityLayer.Concrete;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HotelProject.BusinessLayer.Concrete
 {
 	public class SubscribeManager : ISubscribeService
 	{
-		private readonly ISubscribeService _subscribeService;
+		private readonly ISubscribeDal _subscribeDal;
+
+		public SubscribeManager(ISubscribeDal subscribeDal)
+		{
+			_subscribeDal = subscribeDal;
+		}
+
 		public void TDelete(Subscribe t)
 		{
-			_subscribeService.TDelete(t);
+			_subscribeDal.Delete(t);
 		}
 
 		public Subscribe TGetByID(int id)
 		{
-			return _subscribeService.TGetByID(id);
+			return _subscribeDal.GetByID(id);
 		}
 
 		public List<Subscribe> TGetList()
 		{
-			return _subscribeService.TGetList();
+			return _subscribeDal.GetList();
 		}
 
 		public void TInsert(Subscribe t)
 		{
-			_subscribeService.TInsert(t);
+			_subscribeDal.Insert(t);
 		}
 
 		public void TUpdate(Subscribe t)
 		{
-			_subscribeService.TUpdate(t);
+			_subscribeDal.Update(t);
 		}
 	}
 }
--- MyUdemyProject/ApiConsume/HotelProject.BusinessLayer/Concrete/TestimonialManager.cs
+++ MyUdemyProject/ApiConsume/HotelProject.BusinessLayer/Concrete/TestimonialManager.cs
@@ -1,39 +1,42 @@
 ﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.DataAccessLayer.Abstract;
 using HotelProject.EntityLayer.Concrete;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HotelProject.BusinessLayer.Concrete
 {
 	public class TestimonialManager : ITestimonialService
 	{
-		private readonly ITestimonialService _testimonialService;
+		private readonly ITestimonialDal _testimonialDal;
+
+		public TestimonialManager(ITestimonialDal testimonialDal)
+		{
+			_testimonialDal = testimonialDal;
+		}
+
 		public void TDelete(Testimonial t)
 		{
-			_testimonialService.TDelete(t);
+			_testimonialDal.Delete(t);
 		}
 
 		public Testimonial TGetByID(int id)
 		{
-			return _testimonialService.TGetByID(id);
+			return _testimonialDal.GetByID(id);
 		}
 
 		public List<Testimonial> TGetList()
 		{
-			return _testimonialService.TGetList();
+			return _testimonialDal.GetList();
 		}
 
 		public void TInsert(Testimonial t)
 		{
-			_testimonialService.TInsert(t);
+			_testimonialDal.Insert(t);
 		}
 
 		public void TUpdate(Testimonial t)
 		{
-			_testimonialService.TUpdate(t);
+			_testimonialDal.Update(t);
 		}
 	}
 }
