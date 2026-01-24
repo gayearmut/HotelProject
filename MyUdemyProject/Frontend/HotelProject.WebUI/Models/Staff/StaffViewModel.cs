97d47c544f18c79545e26035fe5a5cdfdbbe5e74
gaye armut <gayearmut@gmail.com>
2026-01-24 12:07:35 +0300

    last edit

---
+++ MyUdemyProject/Frontend/HotelProject.WebUI/Models/Staff/StaffViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelProject.WebUI.Models.Staff
+{
+    public class StaffViewModel
+    {
+        public int StaffID { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+  
+    }
+}
