3ac24e35dbe3e6f07633cd4ebaf03b997b365436
gaye armut <gayearmut@gmail.com>
2026-02-01 19:55:41 +0300

    add view component

---
+++ MyUdemyProject/Frontend/HotelProject.WebUI/ViewComponents/Default/_HeadPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _HeadPartial:ViewComponent 
+    {
+        public IViewComponentResult Invoke() //Default method name is Invoke
+        {
+            return View();
+        }
+    }
+}
---
+++ MyUdemyProject/Frontend/HotelProject.WebUI/ViewComponents/Default/_NavbarPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelProject.WebUI.ViewComponents.Default
+{
+    public class _NavbarPartial:ViewComponent 
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }
+    }
+   
+}
