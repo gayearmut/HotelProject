95146d67238d44ddc55d3aa3db29b204e9087a0f
gaye armut <gayearmut@gmail.com>
2026-02-21 15:46:39 +0300

    add room page

---
+++ MyUdemyProject/Frontend/HotelProject.WebUI/Dtos/RoomDto/CreateRoomDto.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelProject.WebUI.Dtos.RoomDto
+{
+    public class CreateRoomDto
+    {
+    }
+}
--- MyUdemyProject/Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
+++ MyUdemyProject/Frontend/HotelProject.WebUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -1,4 +1,4 @@
-﻿//using HotelProject.WebUI.Dtos.RoomDto;
+﻿using HotelProject.WebUI.Dtos.RoomDto;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System;
@@ -21,9 +21,9 @@ namespace HotelProject.WebUI.ViewComponents.Default
             var responseMessage = await client.GetAsync("http://localhost:5143/api/Room");
             if (responseMessage.IsSuccessStatusCode)
             {
-                //var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                //var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData);
-                //return View(values);
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultRoomDto>>(jsonData);
+                return View(values);
             }
             return View();
         }
