1e5885790e39254d626827c016da2865b7f7b5cc
gaye armut <gayearmut@gmail.com>
2026-02-21 17:07:43 +0300

    add Team Partial

--- MyUdemyProject/Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ MyUdemyProject/Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -8,9 +8,9 @@ using HotelProject.WebUI.Dtos.AboutDto;
 using HotelProject.WebUI.Dtos.LoginDto;
 using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
-//using HotelProject.WebUI.Dtos.StaffDto;
+using HotelProject.WebUI.Dtos.StaffDto;
 //using HotelProject.WebUI.Dtos.SubscribeDto;
-//using HotelProject.WebUI.Dtos.TestimonialDto;
+using HotelProject.WebUI.Dtos.TestimonialDto;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -34,9 +34,9 @@ namespace HotelProject.WebUI.Mapping
             CreateMap<ResultAboutDto, About>().ReverseMap();
             CreateMap<UpdateAboutDto, About>().ReverseMap();
 
-            //CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
+            CreateMap<ResultTestimonialDto, Testimonial>().ReverseMap();
 
-            //CreateMap<ResultStaffDto, Staff>().ReverseMap();
+            CreateMap<ResultStaffDto, Staff>().ReverseMap();
 
             //CreateMap<CreateSubscribeDto, Subscribe>().ReverseMap();
 
