1af4685580e96df3c756739a1bfecd1615b65cff
gaye armut <gayearmut@gmail.com>
2026-03-01 16:48:23 +0300

    add booking exchange

---
+++ MyUdemyProject/RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RapidApiConsume.Controllers
+{
+    public class ExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com15.p.rapidapi.com/api/v1/meta/getExchangeRates?base_currency=TRY"),
+                Headers =
+    {
+        { "x-rapidapi-key", "73e598ec5dmsh8ef4c84c79250d3p14060fjsn2caa205da947" },
+        { "x-rapidapi-host", "booking-com15.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.data.exchange_rates.ToList());
+            }        
+            
+        }
+    }
+}
---
+++ MyUdemyProject/RapidApi/RapidApiConsume/Models/ExchangeViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace RapidApiConsume.Models
+{
+    public class ExchangeViewModel
+    {
+        public bool status { get; set; }
+        public string message { get; set; }
+        public ExchangeData data { get; set; }
+
+        public class ExchangeData
+        {
+            public string base_currency { get; set; }
+            public string base_currency_date { get; set; }
+            public Exchange_Rates[] exchange_rates { get; set; }
+        }
+
+        public class Exchange_Rates
+        {
+            public string exchange_rate_buy { get; set; }
+            public string currency { get; set; }
+        }
+    }
+}
